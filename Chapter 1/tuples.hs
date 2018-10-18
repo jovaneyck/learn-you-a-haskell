2daa69643ec1a7c0b649dd49b79dba7890fe1e42
Jo Van Eyck <jo.vaneyck@ae.be>
2018-10-18 20:44:28 +0200

    tuples

--- Chapter 1/tuples.hs
+++ Chapter 1/tuples.hs
@@ -2,7 +2,23 @@ import Test.HUnit
 :{
 
     tests = 
-        test [  "concatenation" ~: [1] ++ [2] ~=? [1,2]
+        test [
+            "tuples can contain heterogeneous types"
+                ~: (1337, "text", True)
+                ~=? (1337, "text", True),
+            "pairs - fst" ~: 1 ~=? fst (1,2),
+            "pairs - snd" ~: 2 ~=? snd (1,2),
+            "zipping lists"
+                ~: [(1,2),(2,3), (3,4),(4,5)]
+                ~=? zip [1..4] [2..],
+            "triangles"
+               ~: [(6,8,10)]
+               ~=? [ (a,b,c) 
+                        | c <- [1..10], 
+                          b <- [1..c], 
+                          a <- [1..b],
+                          a^2 + b^2 == c^2,
+                          a + b + c == 24]
             ]
 
 :}
