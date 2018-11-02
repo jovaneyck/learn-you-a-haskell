e073989fc5778074f2684edca34f9ce2e6f8c921
Jo Van Eyck <jo.vaneyck@ae.be>
2018-11-02 10:07:16 +0100

    augmented helloworld.hs with how to run without ghc instructions

--- Chapter 8 - IO/helloworld.hs
+++ Chapter 8 - IO/helloworld.hs
@@ -1,5 +1,5 @@
 --compile: stack ghc helloworld.hs
-
+--just run: stack runhaskell helloworld.hs
 --IO actions are only ever executed in context of a function called "main"
 main :: IO () -- for illustration purpose, main is usually never typed
 main =
